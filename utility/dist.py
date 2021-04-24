f398a88d46bedd835c6ea3fe0190d65415873cae
mountwebs <mountwebs@gmail.com>
2021-04-24 23:40:19 +0200

    Added comments

--- utility/dist.py
+++ utility/dist.py
@@ -4,12 +4,15 @@ points = [[3,4,5],[6,3,8],[20,1,2],[8,10,20]]
 currentPoint = [8,10,2]
 
 def dist_3d(a,b):
+  ''' Finds distance between two 3d coordinates '''
+  # Formula: sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
   dist = 0
   for i in range(3):
     dist += (a[i] - b[i])**2
   return math.sqrt(dist)
 
 def get_index_shortest_dist(set_point,points):
+  ''' Returns shortest distance between points '''
   dists = []
   for point in points:
     dists.append(dist_3d(set_point, point))
