f9e8f8e20f3135d220c47534a747a3ab579b7993
mountwebs <mountwebs@gmail.com>
2021-05-14 00:17:13 +0200

    fixed basic summary

--- test.py
+++ test.py
@@ -131,7 +131,16 @@ def time_reporter():
             f.close()
             print(states)
 
-def print_summary(session_selected):
+
+def convert_from_ms( milliseconds ): 
+	seconds, milliseconds = divmod(milliseconds,1000) 
+	minutes, seconds = divmod(seconds, 60) 
+	hours, minutes = divmod(minutes, 60) 
+	seconds = int(seconds + milliseconds/1000)
+	return "{}:{}:{}".format(hours, minutes, seconds)
+
+
+def print_summary():
     state_dict = {}
     with open("log.txt","r") as f:
         lines = f.readlines()
@@ -140,68 +149,16 @@ def print_summary(session_selected):
         if len(lines) - 1 > i:
             entry = parse_log_line(l)
             next_entry = parse_log_line(lines[i+1])
-            diff = time.ticks_diff(next_entry["started"], entry["started"])
+            diff = int(next_entry["started"]) - int(entry["started"])
             if entry["state"] in state_dict:
                 state_dict[entry["state"]] += diff
             else:
                 state_dict[entry["state"]] = diff
 
-    print("\n==Summary==\n")
+    print("\n==Summary==")
     for i, project in enumerate(projects):
-        if state_dict[str(i)]:
-            print(project + ": " + state_dict[str(i)])
+        if str(i) in state_dict:
+            time_str = convert_from_ms(int(state_dict[str(i)]))
+
+            print(project + ": " + time_str) 
             
-
-
-def testing():
-    current_orientation = None
-    prev_orientation = None
-
-    while True:
-        data = get_smoothed_values(n_samples=100)
-        # print(
-        #     '\t'.join('{0}:{1:>10.1f}'.format(k, data[k])
-        #               for k in sorted(data.keys())),
-        #     end='\r')
-
-        prev_orientation = current_orientation
-
-        if data["AcX"] > 10000:
-            current_orientation = "right"
-
-        if data["AcX"] < 10000 and data["AcX"] > -10000:
-            current_orientation = "flat"
-
-        if data["AcX"] < -10000:
-            current_orientation = "left"
-
-        print(current_orientation)
-
-        def sendRight():
-            print("r")
-            setRGB(255, 0, 0)
-
-        def sendLeft():
-            print("l")
-            setRGB(0, 255, 0)
-
-        def sendFlat():
-            print("f")
-            setRGB(0, 0, 255)
-
-        action = {
-            'right': sendRight,
-            'flat': sendFlat,
-            'left': sendLeft,
-        }
-
-        if current_orientation != prev_orientation:
-            print("changed orientation!")
-            # compute time difference
-            delta = time.ticks_diff(time.ticks_ms(), start)
-            # if delta > 5000:
-            action[current_orientation]()
-
-            start = time.ticks_ms()  # get millisecond counter
-
-        print(data)
